686f3eaa8a6ce19084b355202274c52e4fe8de85
Kazuki Ochiai <kazuki971007@gmail.com>
2026-01-14 22:25:19 +0900

    first commit

---
+++ lib.rs
@@ -0,0 +1,10 @@
+// 将来ここに pub fn run(...) を置く想定
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // ここは後で clap で引数パースして実行する
+    println!("minidiff: scaffold ok");
+    Ok(())
+}
